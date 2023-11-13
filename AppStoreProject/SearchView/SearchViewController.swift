0d6227cfd4776c6782c05c1e577704b8e1acc879
ha-ny <hany01.dev@gmail.com>
2023-11-13 10:49:05 +0900

    SearchViewController -> searchController 생성

--- AppStoreProject/SearchView/SearchViewController.swift
+++ AppStoreProject/SearchView/SearchViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 final class SearchViewController: UIViewController {
 
+    lazy var searchController = {
+        let view = UISearchController()
+        view.searchBar.placeholder = "게임, 앱, 스토리 등"
+        view.searchBar.setValue("취소", forKey: "cancelButtonText")
+        view.searchBar.showsCancelButton = true
+        return view
+    }()
+    
     private let mainView = CommendView()
     private let apiManager = APIManager.shared
     private var data: SearchApp? {
@@ -16,29 +24,57 @@ final class SearchViewController: UIViewController {
             mainView.commendTableView.reloadData()
         }
     }
+
+    init(text: String) {
+        super.init(nibName: nil, bundle: nil)
+        searchController.searchBar.text = text
+        dataSetting(text: text)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
+        super.loadView()
         self.view = mainView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        navigationItem.title = "검색"
+        searchController.searchBar.delegate = self
+        navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
-        
-        mainView.commendTableHeaderView.isHidden = true
+
         mainView.commendTableView.delegate = self
         mainView.commendTableView.dataSource = self
         mainView.commendTableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
         mainView.commendTableView.rowHeight = 330
-       
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        searchController.searchBar.becomeFirstResponder()
+    }
+
     
     func dataSetting(text: String) {
         apiManager.requestAPI(keyWord: text, limit: 25, completion: { data in
             self.data = data
         })
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text else { return }
+        dataSetting(text: text)
+        mainView.commendTableView.scrollsToTop = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        dismiss(animated: false)
     }
 }
 
@@ -64,7 +100,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         cell.selectionStyle = .none
         
         let rating = String(format:"%.1f", result.averageUserRating)
-        cell.ratingLabel.text = rating
+       cell.ratingLabel.text = rating
         
         //라벨 이미지
         let attributedString = NSMutableAttributedString()
@@ -74,11 +110,11 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         let imageString = NSAttributedString(attachment: imageAttachment)
         attributedString.append(imageString)
        
-       let textString = NSAttributedString(string: " " + result.sellerName) // Add a space before the text
+       let textString = NSAttributedString(string: " " + result.sellerName)
        attributedString.append(textString)
        cell.sellerNameLabel.attributedText = attributedString
 
-        cell.genreLabel.text = result.genres[0]
+       cell.genreLabel.text = result.genres[0]
         
         for (index, stringURL) in result.screenshotUrls.enumerated() {
             guard index < 3 else { break }
@@ -109,5 +145,11 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         cell.selectionStyle = .none
     
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let data else { return }
+        let vc = DetailViewController(trackId: data.results[indexPath.item].trackId)
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
