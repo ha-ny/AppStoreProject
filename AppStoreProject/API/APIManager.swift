297d89546ecb05afc1da5a27ad4b62fe483df002
ha-ny <hany01.dev@gmail.com>
2023-11-14 13:55:08 +0900

    URLSession 생성

--- AppStoreProject/API/APIManager.swift
+++ AppStoreProject/API/APIManager.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Alamofire
 
 final class APIManager {
     
@@ -17,15 +16,18 @@ final class APIManager {
     func requestAPI(keyWord: String, limit: Int, completion: @escaping (SearchApp?) -> ()){
         let term =  keyWord.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         let stringURL = "https://itunes.apple.com/search?term=\(term)&contry=KR&media=software&lang=ko&limit=\(limit)"
-        
-        AF.request(stringURL).responseDecodable(of: SearchApp.self) { request in
-            switch request.result {
-            case .success(let value):
-                completion(value)
-            case .failure(_):
-                completion(nil)
-            }
+
+        guard let url = URL(string: stringURL) else { return completion(nil) }
+
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { (data, response, error) in
+            guard let data else { return completion(nil) }
+
+            do {
+                let resultData = try JSONDecoder().decode(SearchApp.self, from: data)
+                completion(resultData)
+            } catch { }
         }
+        task.resume()
     }
 
 }
